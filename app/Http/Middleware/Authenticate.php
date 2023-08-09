82f0002b8a2479a9de4d25877d2bb7b9baced879
m.andronov <m.andronov@m-games-ltd.com>
2023-08-09 22:55:47 +0300

    init project

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo(Request $request): ?string
+    {
+        return $request->expectsJson() ? null : route('login');
+    }
+
+    public function handle($request, Closure $next, ...$guards)
+    {
+        $token = $request->cookie('accessToken');
+
+        if(!$token) {
+            $token = $request->input('accessToken');
+        }
+
+        if ($token) {
+            $request->headers->set(
+                'Authorization',
+                'Bearer ' . $token
+            );
+        }
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
+    }
+}
